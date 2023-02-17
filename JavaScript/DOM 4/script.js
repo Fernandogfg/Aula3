81367fae6e6304b5cd951b7e07fbe0f99cdad044
Fernando <fernandofgfg10@gmail.com>
2023-02-16 23:30:00 -0400

    DOM 4

---
+++ JavaScript/DOM 4/script.js
@@ -0,0 +1,53 @@
+let nome1 = document.getElementById("nome1");
+let nome2 = document.getElementById("nome2");
+let nome3 = document.getElementById("nome3");
+let arrNomes = [nome1, nome2, nome3]
+let btnSalvar = document.getElementById("salvar");
+let listaSalvos = document.getElementById("lista-salvos");
+let msgErro = document.getElementById("msg-erro");
+let msgSucesso = document.getElementById('msg-sucesso');
+function camposPreenchidos() {
+  if (
+    nome1.value.trim() !== "" &&
+    nome2.value.trim() !== "" &&
+    nome3.value.trim() !== ""
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function enviaErro() {
+    apagaSucesso()
+  msgErro.style.display = "block";
+}
+function apagaErro(){
+    msgErro.style.display = 'none'
+}
+function enviaSucesso () {
+    msgSucesso.style.display = 'block'
+}
+function apagaSucesso () {
+    msgSucesso.style.display = 'none'
+}
+function preencheLista (){
+   for(let i = 0; i < arrNomes.length; i++){
+    listaSalvos.innerHTML += `<p>Nome${i+1}: <strong>${arrNomes[i].value}</strong></p>`
+   }
+    
+}
+function limpaLista (){
+    listaSalvos.innerHTML = '';
+}
+function salvar() {
+  if (camposPreenchidos()) {
+    apagaErro()
+    limpaLista()
+    preencheLista()
+    enviaSucesso()
+  } else {
+    enviaErro();
+  }
+}
+btnSalvar.onclick = salvar;
