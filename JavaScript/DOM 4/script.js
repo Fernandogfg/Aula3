38e3ba988d43b85fb316e2c55ae614bf00b99f62
Fernando <fernandofgfg10@gmail.com>
2023-02-19 01:00:03 -0400

    cleaning inputs

--- JavaScript/DOM 4/script.js
+++ JavaScript/DOM 4/script.js
@@ -1,11 +1,11 @@
 let nome1 = document.getElementById("nome1");
 let nome2 = document.getElementById("nome2");
 let nome3 = document.getElementById("nome3");
-let arrNomes = [nome1, nome2, nome3]
+let arrNomes = [nome1, nome2, nome3];
 let btnSalvar = document.getElementById("salvar");
 let listaSalvos = document.getElementById("lista-salvos");
 let msgErro = document.getElementById("msg-erro");
-let msgSucesso = document.getElementById('msg-sucesso');
+let msgSucesso = document.getElementById("msg-sucesso");
 function camposPreenchidos() {
   if (
     nome1.value.trim() !== "" &&
@@ -19,33 +19,41 @@ function camposPreenchidos() {
 }
 
 function enviaErro() {
-    apagaSucesso()
+  apagaSucesso();
   msgErro.style.display = "block";
 }
-function apagaErro(){
-    msgErro.style.display = 'none'
+function apagaErro() {
+  msgErro.style.display = "none";
 }
-function enviaSucesso () {
-    msgSucesso.style.display = 'block'
+function enviaSucesso() {
+  msgSucesso.style.display = "block";
 }
-function apagaSucesso () {
-    msgSucesso.style.display = 'none'
+function apagaSucesso() {
+  msgSucesso.style.display = "none";
 }
-function preencheLista (){
-   for(let i = 0; i < arrNomes.length; i++){
-    listaSalvos.innerHTML += `<p>Nome${i+1}: <strong>${arrNomes[i].value}</strong></p>`
-   }
-    
+function preencheLista() {
+  for (let i = 0; i < arrNomes.length; i++) {
+    listaSalvos.innerHTML += `<p>Nome${i + 1}: <strong>${
+      arrNomes[i].value
+    }</strong></p>`;
+  }
 }
-function limpaLista (){
-    listaSalvos.innerHTML = '';
+function limpaLista() {
+  listaSalvos.innerHTML = "";
+}
+
+function limpaCampos() {
+  nome1.value = "";
+  nome2.value = "";
+  nome3.value = "";
 }
 function salvar() {
   if (camposPreenchidos()) {
-    apagaErro()
-    limpaLista()
-    preencheLista()
-    enviaSucesso()
+    apagaErro();
+    limpaLista();
+    preencheLista();
+    enviaSucesso();
+    limpaCampos()
   } else {
     enviaErro();
   }
