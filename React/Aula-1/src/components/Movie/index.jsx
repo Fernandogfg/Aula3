35ebd94356e9795988efd526004e2217e78f9cef
Fernando <fernandofgfg10@gmail.com>
2023-05-17 20:05:33 -0400

    fix

--- React/Aula-1/src/components/Movie/index.jsx
+++ React/Aula-1/src/components/Movie/index.jsx
@@ -1,20 +1,18 @@
 function Movie(prop) {
   return (
     <div>
-        {prop.obj.map(filme =>(
-            <div>
-                <h2>{filme.title}</h2>
-                <ul>
-                    <li>Diretor: {filme.diretor}</li>
-                    <li>Ano de lançamento: {filme.ano}</li>
-                    <li>Duração: {filme.tempo}</li>
-                </ul>
-            </div>
-        
-    ))}
-    </div>)}
-      
-  ;
-
+      {prop.obj.map((filme) => (
+        <div>
+          <h2>{filme.title}</h2>
+          <ul>
+            <li>Diretor: {filme.diretor}</li>
+            <li>Ano de lançamento: {filme.ano}</li>
+            <li>Duração: {filme.tempo}</li>
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
+}
 
 export default Movie;
